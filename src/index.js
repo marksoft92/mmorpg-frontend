1d6ef1969f3953dc5f47981728ad1fc97c8a928a
Dawid BIeńkowski <marksoft1992@gmail.com>
2023-09-13 20:58:14 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// Importujemy moduł socket.io-client
+import io from 'socket.io-client';
+
+// Adres i port serwera WebSocket
+const serverUrl = 'http://localhost:3003';
+ // Zastąp adresem i portem swojego serwera
+
+// Połącz się z serwerem WebSocket
+const socket = io(serverUrl);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const connectButton = document.getElementById('connectButton');
+  const updateButton = document.getElementById('updateButton');
+
+  connectButton.addEventListener('click', () => {
+   
+    socket.connect(); // Połącz z serwerem WebSocket
+    console.log('Połączono z serwerem WebSocket', socket.connect());
+ 
+  });
+
+  updateButton.addEventListener('click', () => {
+    // Wysyłamy żądanie aktualizacji danych postaci
+    const characterId = 0; // Zastąp ID swoją własną wartością
+    const newData = {
+      level: 1110,
+      experience: 500,
+      health: 80,
+      stats: {
+        agility: 15,
+        vitality: 25,
+      },
+    };
+console.log(socket);
+    socket.emit('updateCharacter', { characterId, newData });
+  });
+
+  // Nasłuchiwanie na zdarzenia zwrotne, np. "characterUpdated"
+  socket.on('characterUpdated', (updatedCharacter) => {
+    console.log('Zaktualizowane dane postaci:', updatedCharacter);
+  });
+
+  // Obsługa rozłączenia
+  socket.on('disconnect', () => {
+    console.log('Rozłączono z serwerem WebSocket');
+  });
+});
